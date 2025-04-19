03fd6658433a0b743b3b960e4d7107622aa834f5
Ankur <singhankur362@gmail.com>
2025-04-20 02:47:28 +0530

    Made changes in landing page

--- Client/src/pages/Profile.jsx
+++ Client/src/pages/Profile.jsx
@@ -1,39 +1,50 @@
 import React, { useState, useEffect } from "react";
-import axios from 'axios';
-import {useSelector} from 'react-redux';
-import UserProfile from "../components/ProfileMainComponent"
+import axios from "axios";
+import { useSelector } from "react-redux";
+import UserProfile from "../components/ProfileMainComponent";
 import ProfileTabContent from "../components/profile_components/ProfileTabContent";
 
 const Profile = () => {
-    const [activeTab, setActiveTab] = useState("Posts");
-    const LoggedInUser = useSelector((state) => state.user);
-    const [posts, setPosts] = useState([]);
+  const [activeTab, setActiveTab] = useState("Posts");
+  const LoggedInUser = useSelector((state) => state.user);
+  const [posts, setPosts] = useState([]);
 
-    useEffect(() => {
-        const fetchPosts = async () => {
-            try {
-                const response = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/api/posts/u`, {
-                    withCredentials : true
-                });
-                console.log(response);
-                if (!response.data.success) {
-                  throw new Error(`Error: ${response.status}`);
-                }
-                
-                setPosts(response.data.posts);
-            } catch (err) {
-                console.error("Error fetching posts:", err);
-            }
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await axios.get(
+          `${import.meta.env.VITE_BACKEND_URL}/api/posts/u`,
+          {
+            withCredentials: true,
+          }
+        );
+        console.log(response);
+        if (!response.data.success) {
+          throw new Error(`Error: ${response.status}`);
         }
-        fetchPosts();
-    }, []);
 
-    return (
-        <div className="p-4">
-            <UserProfile activeTab={activeTab} setActiveTab={setActiveTab} LoggedInUser={LoggedInUser}/>
-            <ProfileTabContent activeTab={activeTab} posts={posts} LoggedInUser={LoggedInUser}/>
-        </div>
-    );
+        setPosts(response.data.posts);
+      } catch (err) {
+        console.error("Error fetching posts:", err);
+      }
+    };
+    fetchPosts();
+  }, []);
+
+  return (
+    <div className="p-4">
+      <UserProfile
+        activeTab={activeTab}
+        setActiveTab={setActiveTab}
+        LoggedInUser={LoggedInUser}
+      />
+      <ProfileTabContent
+        activeTab={activeTab}
+        posts={posts}
+        LoggedInUser={LoggedInUser}
+      />
+    </div>
+  );
 };
 
 export default Profile;
